7dd6b59c5b5136cb2e246392f0ff4184df902af1
Tobias Schultheiß <ts@vioma.de>
2020-03-16 13:37:36 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Installing needed software..."
 apt install jq
 apt install curl
 apt install wget
@@ -12,8 +13,10 @@ rm $dir/KittyBot.jar
 
 url=$(curl -sL https://api.github.com/repos/TopiSenpai/KittyBot/releases/latest | jq -r '.assets[].browser_download_url')
 
+echo "Downloading .jar file..."
 wget -O KittyBot.jar $url
 
+echo "Adding service file..."
 echo "[Unit]
 Description=KittyBot Service
 After=mysqld.service
@@ -21,15 +24,20 @@ After=mysqld.service
 [Service]
 Type=simple
 User=$USER
+WorkingDirectory=$dir
 ExecStart=$dir/start.sh
 
 [Install]
 WantedBy=multi-user.target" >> /etc/systemd/system/kittybot.service
 
+echo "Adding start script..."
 echo "#!/bin/bash
 
 java -jar $dir/KittyBot.jar" >> start.sh
 
 chmod +x start.sh
 
+echo "Reloading services..."
 systemctl daemon-reload
+
+echo "Run 'systemctl start kittybot' to start the bot!"
