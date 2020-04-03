1cbbd58aac03a5bd11df4ae7982093227b8c22b5
ToπSenpai <tobi@strasserhof.com>
2020-04-04 00:10:35 +0200

    added permission, some checks and ability to provide emote id

--- src/main/java/de/anteiku/kittybot/commands/EmoteStealCommand.java
+++ src/main/java/de/anteiku/kittybot/commands/EmoteStealCommand.java
@@ -2,6 +2,7 @@ package de.anteiku.kittybot.commands;
 
 import de.anteiku.kittybot.KittyBot;
 import de.anteiku.kittybot.utils.Logger;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Emote;
 import net.dv8tion.jda.api.entities.Icon;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -13,7 +14,7 @@ import java.util.List;
 public class EmoteStealCommand extends ACommand{
 
 	public static String COMMAND = "steal";
-	public static String USAGE = "steal <Emote, Emote, Emote, ...>";
+	public static String USAGE = "steal <Emote/ID, ...>";
 	public static String DESCRIPTION = "Steals some emotes";
 	protected static String[] ALIAS = {"grab", "klau"};
 
@@ -24,22 +25,46 @@ public class EmoteStealCommand extends ACommand{
 
 	@Override
 	public void run(String[] args, GuildMessageReceivedEvent event){
+		if(!event.getMember().hasPermission(Permission.MANAGE_EMOTES)){
+			sendError(event.getMessage(), "Sorry you don't have the permission to manage emotes :(");
+			return;
+		}
 		List<Emote> emotes = event.getMessage().getEmotes();
+		for(String arg : args) {
+			try{
+				Long.parseLong(arg);
+				emotes.add(event.getGuild().retrieveEmoteById(arg).complete());
+			}
+			catch(NumberFormatException e) {
+				Logger.error(e);
+			}
+		}
 		if(!emotes.isEmpty()) {
 			boolean success = true;
 			for(Emote emote : emotes) {
+				if(emote.getGuild().getId().equals(event.getGuild().getId())) {
+					continue;
+				}
 				try{
 					Icon icon = Icon.from(new URL(emote.getImageUrl()).openStream());
 					event.getGuild().createEmote(emote.getName(), icon).queue();
 				}
 				catch(IOException e){
 					Logger.error(e);
-					sendAnswer(event.getChannel(), "There was a problem stealing " + emote.getAsMention());
+					sendError(event.getMessage(), "There was a problem stealing " + emote.getAsMention());
 					success = false;
 				}
 			}
 			if(success) {
-				sendAnswer(event.getChannel(), "All emotes stolen!");
+				StringBuilder msg = new StringBuilder("Following emote");
+				if(emotes.size() > 1) {
+					msg.append("s");
+				}
+				msg.append(" stolen: \n");
+				for(Emote emote : emotes) {
+					msg.append(emote.getAsMention()).append(" ");
+				}
+				sendAnswer(event.getMessage(), msg.toString());
 			}
 		}
 		else {
