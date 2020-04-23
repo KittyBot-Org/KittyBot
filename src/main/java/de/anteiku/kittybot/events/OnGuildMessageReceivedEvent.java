7cff21588ca7b52896bdbd218566cff6957b32d7
ToπSenpai <tobi@strasserhof.com>
2020-04-23 23:42:58 +0200

    typo

--- src/main/java/de/anteiku/kittybot/events/OnGuildMessageReceivedEvent.java
+++ src/main/java/de/anteiku/kittybot/events/OnGuildMessageReceivedEvent.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.awt.*;
+import java.time.Instant;
 
 public class OnGuildMessageReceivedEvent extends ListenerAdapter{
 	
@@ -24,14 +25,19 @@ public class OnGuildMessageReceivedEvent extends ListenerAdapter{
 		if(!main.commandManager.checkCommands(event)){
 			if(event.getMessage().getMentionedUsers().size() == 1 && event.getMessage().getMentionedUsers().get(0).getId().equals(main.jda.getSelfUser().getId())) {
 				event.getMessage().addReaction(Emotes.QUESTION.get()).queue();
-				event.getChannel().sendMessage(
-					new EmbedBuilder()
-						.setColor(Color.ORANGE)
-						.setTitle("Do you need help?")
-						.setDescription("Do you want to know what my prefix is?\n" +
-							"It is `" + main.database.getCommandPrefix(event.getGuild().getId()) + "`\n" +
-							"If you need more help run `" + main.database.getCommandPrefix(event.getGuild().getId()) + "help` or `" + main.database.getCommandPrefix(event.getGuild().getId()) + "commands`" + Emotes.KITTY_BLINK.get())
-						.build()
+				event.getChannel().sendMessage(new EmbedBuilder()
+					.setColor(Color.ORANGE)
+					.setTitle("Do you need help?")
+					.setDescription(
+						"My current prefix for this guild is `" + main.database.getCommandPrefix(event.getGuild().getId()) + "`\n" +
+						"If you don't like my prefix you can ping me directly!\n" +
+						"To have a look at all my commands use `" + main.database.getCommandPrefix(event.getGuild().getId()) + "cmds`\n" +
+						"To get help use`" + main.database.getCommandPrefix(event.getGuild().getId()) + "help`"
+					)
+					.setThumbnail(event.getJDA().getSelfUser().getAvatarUrl())
+					.setFooter(event.getMember().getEffectiveName(), event.getAuthor().getEffectiveAvatarUrl())
+					.setTimestamp(Instant.now())
+					.build()
 				).queue();
 			}
 		}
