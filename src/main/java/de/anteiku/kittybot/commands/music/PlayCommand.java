f8f6c3f7b3f2a6b3b0e61ead53a672507c788ea7
TopiSenpai <tobi@strasserhof.com>
2020-08-16 00:24:11 +0200

    cleaner check

--- src/main/java/de/anteiku/kittybot/commands/music/PlayCommand.java
+++ src/main/java/de/anteiku/kittybot/commands/music/PlayCommand.java
@@ -32,19 +32,17 @@ public class PlayCommand extends ACommand{
 		}
 		GuildVoiceState voiceState = ctx.getMember().getVoiceState();
 		if(voiceState != null && voiceState.inVoiceChannel()){
-			var currentPlayer = Cache.getMusicPlayer(ctx.getGuild());
-			if (currentPlayer != null){
-				currentPlayer.loadItem(this, ctx, ctx.getArgs());
-				return;
-			}
-			JdaLink link = KittyBot.getLavalink().getLink(ctx.getGuild());
-			link.connect(voiceState.getChannel());
+			var musicPlayer = Cache.getMusicPlayer(ctx.getGuild());
+			if(musicPlayer == null){
+				JdaLink link = KittyBot.getLavalink().getLink(ctx.getGuild());
+				link.connect(voiceState.getChannel());
 
-			LavalinkPlayer player = link.getPlayer();
-			MusicPlayer newPlayer = new MusicPlayer(player);
-			player.addListener(newPlayer);
-			Cache.addMusicPlayer(ctx.getGuild(), newPlayer);
-			newPlayer.loadItem(this, ctx, ctx.getArgs());
+				LavalinkPlayer player = link.getPlayer();
+				musicPlayer = new MusicPlayer(player);
+				player.addListener(musicPlayer);
+				Cache.addMusicPlayer(ctx.getGuild(), musicPlayer);
+			}
+			musicPlayer.loadItem(this, ctx, ctx.getArgs());
 		}
 		else{
 			sendError(ctx, "Please connect to a voice channel to play some stuff");
