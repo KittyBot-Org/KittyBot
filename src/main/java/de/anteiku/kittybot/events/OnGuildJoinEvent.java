e5a1cf2c11c3e9f84bd8f5d5e452d2637eeec6fe
ToπSenpai <15636011+TopiSenpai@users.noreply.github.com>
2020-08-26 10:52:13 +0200

    hotfix: load invites when joining a new guild

--- src/main/java/de/anteiku/kittybot/events/OnGuildJoinEvent.java
+++ src/main/java/de/anteiku/kittybot/events/OnGuildJoinEvent.java
@@ -1,6 +1,7 @@
 package de.anteiku.kittybot.events;
 
 import de.anteiku.kittybot.database.Database;
+import de.anteiku.kittybot.objects.Cache;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.audit.ActionType;
 import net.dv8tion.jda.api.audit.AuditLogEntry;
@@ -17,6 +18,7 @@ public class OnGuildJoinEvent extends ListenerAdapter{
 	@Override
 	public void onGuildJoin(GuildJoinEvent event){
 		Database.registerGuild(event.getGuild());
+		Cache.initGuildInviteCache(event.getGuild());
 		for(AuditLogEntry entry : event.getGuild().retrieveAuditLogs().cache(false)){
 			if(entry.getType() == ActionType.BOT_ADD && entry.getTargetId().equals(event.getJDA().getSelfUser().getId())){
 				MessageEmbed embed = new EmbedBuilder().setTitle("Hellowo and thank your for adding me to your Discord Server!").setDescription("To get started you maybe want to set up some self assignable roles. This can be done with `.roles add @role :emote:`. You will need a emote for each role and they should be from your server!\n\n" + "If you want to know my other commands just type ``.commands``.\n" + "To change my prefix use ``.options prefix <your wished prefix>``.\n" + "In case you forgot any command just type ``.cmds`` to get a full list off all my commands!\n\n" + "To report bugs/suggest features either join my [Support Server](https://discord.gg/sD3ABd5), add me on Discord ``Toπ#3141`` or message me on [Twitter](https://twitter.com/TopiSenpai)").setColor(new Color(76, 80, 193)).setThumbnail(event.getJDA().getSelfUser().getEffectiveAvatarUrl()).setFooter(event.getGuild().getName(), event.getGuild().getIconUrl()).setTimestamp(Instant.now()).build();
