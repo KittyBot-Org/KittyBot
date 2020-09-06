48f90d8bceb08f8893a0bf5ed57f96dc9b7ae103
caneleex <git@mlnr.dev>
2020-09-06 22:34:32 +0200

    remove 's' alias for stop command

--- src/main/java/de/anteiku/kittybot/commands/music/StopCommand.java
+++ src/main/java/de/anteiku/kittybot/commands/music/StopCommand.java
@@ -10,7 +10,7 @@ public class StopCommand extends ACommand{
 	public static final String COMMAND = "stop";
 	public static final String USAGE = "stop";
 	public static final String DESCRIPTION = "Stops me from playing stuff";
-	protected static final String[] ALIASES = {"s", "quit", "stopp", "stfu"};
+	protected static final String[] ALIASES = {"quit", "stopp", "stfu"};
 	protected static final Category CATEGORY = Category.MUSIC;
 
 	public StopCommand(){
