38549edf324fd5e96d089bd04820ea611e92ae4a
caneleex <git@mlnr.dev>
2020-09-29 19:59:46 +0200

    added jda-nas and other shit lol

--- src/main/java/de/kittybot/kittybot/KittyBot.java
+++ src/main/java/de/kittybot/kittybot/KittyBot.java
@@ -1,6 +1,7 @@
 package de.kittybot.kittybot;
 
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import com.sedmelluq.discord.lavaplayer.jdaudp.NativeAudioSendFactory;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
@@ -24,7 +25,6 @@ import net.dv8tion.jda.api.*;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.RestAction;
-import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import okhttp3.OkHttpClient;
@@ -91,9 +91,7 @@ public class KittyBot{
 					GatewayIntent.GUILD_MESSAGES,
 					GatewayIntent.GUILD_MESSAGE_REACTIONS,
 					GatewayIntent.GUILD_EMOJIS,
-					GatewayIntent.GUILD_INVITES,
-					GatewayIntent.DIRECT_MESSAGES,
-					GatewayIntent.DIRECT_MESSAGE_REACTIONS
+					GatewayIntent.GUILD_INVITES
 			)
 					.disableCache(
 							CacheFlag.MEMBER_OVERRIDES,
@@ -101,7 +99,6 @@ public class KittyBot{
 							CacheFlag.CLIENT_STATUS
 					)
 					.setMemberCachePolicy(MemberCachePolicy.ALL)
-					.setChunkingFilter(ChunkingFilter.ALL)
 					.setToken(Config.BOT_TOKEN)
 					.addEventListeners(
 							new OnEmoteEvent(),
@@ -119,6 +116,7 @@ public class KittyBot{
 					.setActivity(Activity.playing("loading..."))
 					.setStatus(OnlineStatus.DO_NOT_DISTURB)
 					.setGatewayEncoding(GatewayEncoding.ETF)
+					.setAudioSendFactory(new NativeAudioSendFactory())
 					.build().awaitReady();
 
 
