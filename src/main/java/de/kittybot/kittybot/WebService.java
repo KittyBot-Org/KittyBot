70ddfca0975218c81027feffa44e266302e5bf65
TopiSenpai <tobi@strasserhof.com>
2020-10-07 19:09:53 +0200

    reformat

--- src/main/java/de/kittybot/kittybot/WebService.java
+++ src/main/java/de/kittybot/kittybot/WebService.java
@@ -25,7 +25,6 @@ import java.io.IOException;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
