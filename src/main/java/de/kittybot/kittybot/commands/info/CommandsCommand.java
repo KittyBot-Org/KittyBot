a78f1f2afcc1efc1dc8aa979ccae4d2c6e08cea0
Sebo Molnár <git@mlnr.dev>
2020-10-26 10:23:47 +0100

    replace paginator with a link to the website (#150)

--- src/main/java/de/kittybot/kittybot/commands/info/CommandsCommand.java
+++ src/main/java/de/kittybot/kittybot/commands/info/CommandsCommand.java
@@ -1,21 +1,9 @@
 package de.kittybot.kittybot.commands.info;
 
-import de.kittybot.kittybot.cache.GuildSettingsCache;
-import de.kittybot.kittybot.objects.Emojis;
-import de.kittybot.kittybot.objects.TitleInfo;
 import de.kittybot.kittybot.objects.command.ACommand;
 import de.kittybot.kittybot.objects.command.Category;
 import de.kittybot.kittybot.objects.command.CommandContext;
-import de.kittybot.kittybot.objects.command.CommandManager;
-import de.kittybot.kittybot.objects.paginator.Paginator;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.MessageEmbed;
-
-import java.awt.*;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
+import de.kittybot.kittybot.utils.MessageUtils;
 
 public class CommandsCommand extends ACommand{
 
@@ -31,41 +19,7 @@ public class CommandsCommand extends ACommand{
 
 	@Override
 	public void run(CommandContext ctx){
-		final var channel = ctx.getChannel();
-		final var message = ctx.getMessage();
-		final var selfMember = channel.getGuild().getSelfMember();
-		if(!channel.canTalk()){
-			if(selfMember.hasPermission(channel, Permission.MESSAGE_ADD_REACTION, Permission.MESSAGE_HISTORY)){
-				message.addReaction(Emojis.X).queue();
-			}
-			return;
-		}
-		if(!selfMember.hasPermission(channel, Permission.MESSAGE_HISTORY) || !selfMember.hasPermission(channel, Permission.MESSAGE_ADD_REACTION) || !selfMember.hasPermission(channel, Permission.MESSAGE_MANAGE)){
-			channel.sendMessage(new EmbedBuilder().setColor(Color.RED)
-					.addField("Error:", "I'm missing required permissions for paginator to work. Ensure that i can read the message history, add reactions and manage messages in this channel.", true)
-					.setFooter(ctx.getMember().getEffectiveName(), ctx.getUser().getEffectiveAvatarUrl())
-					.setTimestamp(Instant.now())
-					.build()).queue(); // TODO improve checks
-			return;
-		}
-		final var titles = new HashMap<Integer, TitleInfo>();
-		final var contents = new HashMap<Integer, ArrayList<MessageEmbed.Field>>();
-
-		final var prefix = GuildSettingsCache.getCommandPrefix(ctx.getGuild().getId());
-		final var commands = CommandManager.getDistinctCommands().values();
-		final var categories = Category.values();
-		var c = 0;
-		for(final var category : categories){
-			titles.put(c, new TitleInfo(category.getEmote() + " " + category.getFriendlyName(), category.getUrl()));
-
-			final var fields = new ArrayList<MessageEmbed.Field>();
-			commands.stream()
-					.filter(command -> command.getCategory() == category)
-					.forEach(cmd -> fields.add(new MessageEmbed.Field("**" + prefix + cmd.getCommand() + ":** ", "• " + cmd.getDescription(), true)));
-			contents.put(c, fields);
-			c++;
-		}
-		Paginator.createCommandsPaginator(message, categories.length, titles, contents);
+		sendAnswer(ctx, "You can see all available commands " + MessageUtils.maskLink("here", "https://kittybot.de/commands"));
 	}
 
 }
