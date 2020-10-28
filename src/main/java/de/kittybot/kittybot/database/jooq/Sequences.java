ea0e3ecebda4588fe6817734e8e2143486ede0f2
TopiSenpai <tobi@strasserhof.com>
2020-10-28 21:09:19 +0100

    regenerated jooq

--- src/main/java/de/kittybot/kittybot/database/jooq/Sequences.java
+++ src/main/java/de/kittybot/kittybot/database/jooq/Sequences.java
@@ -11,12 +11,11 @@ import org.jooq.impl.Internal;
 /**
  * Convenience access to all sequences in public
  */
-@SuppressWarnings({"all", "unchecked", "rawtypes"})
-public class Sequences{
+@SuppressWarnings({ "all", "unchecked", "rawtypes" })
+public class Sequences {
 
-	/**
-	 * The sequence <code>public.self_assignable_role_groups_group_id_seq</code>
-	 */
-	public static final Sequence<Integer> SELF_ASSIGNABLE_ROLE_GROUPS_GROUP_ID_SEQ = Internal.createSequence("self_assignable_role_groups_group_id_seq", Public.PUBLIC, org.jooq.impl.SQLDataType.INTEGER.nullable(false), null, null, null, null, false, null);
-
+    /**
+     * The sequence <code>public.self_assignable_role_groups_group_id_seq</code>
+     */
+    public static final Sequence<Integer> SELF_ASSIGNABLE_ROLE_GROUPS_GROUP_ID_SEQ = Internal.createSequence("self_assignable_role_groups_group_id_seq", Public.PUBLIC, org.jooq.impl.SQLDataType.INTEGER.nullable(false), null, null, null, null, false, null);
 }
