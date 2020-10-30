71fa99488e913a3c830f251840d7c4d16c4b3a9d
ToπSenpai <15636011+TopiSenpai@users.noreply.github.com>
2020-10-30 14:39:42 +0100

    more info commands (#163)
    * Update Utils.java
    
    * Create PingCommand.java
    
    * Create UptimeCommand.java

---
+++ src/main/java/de/kittybot/kittybot/commands/info/PingCommand.java
@@ -0,0 +1,32 @@
+package de.kittybot.kittybot.commands.info;
+
+import de.kittybot.kittybot.objects.Config;
+import de.kittybot.kittybot.objects.command.ACommand;
+import de.kittybot.kittybot.objects.command.Category;
+import de.kittybot.kittybot.objects.command.CommandContext;
+import net.dv8tion.jda.api.EmbedBuilder;
+
+public class PingCommand extends ACommand{
+
+	public static final String COMMAND = "ping";
+	public static final String USAGE = "ping";
+	public static final String DESCRIPTION = "Shows the bots ping";
+	protected static final String[] ALIASES = {};
+	protected static final Category CATEGORY = Category.INFORMATIVE;
+
+	public PingCommand(){
+		super(COMMAND, USAGE, DESCRIPTION, ALIASES, CATEGORY);
+	}
+
+	@Override
+	public void run(CommandContext ctx){
+		var jda = ctx.getJDA();
+		sendAnswer(ctx, new EmbedBuilder()
+			                .setAuthor("KittyBot Ping", Config.ORIGIN_URL, jda.getSelfUser().getEffectiveAvatarUrl())
+
+			                .addField("Gateway Ping:", jda.getGatewayPing() + "ms", false)
+			                .addField("Rest Ping:", jda.getRestPing().complete() + "ms", false)
+		);
+	}
+
+}
