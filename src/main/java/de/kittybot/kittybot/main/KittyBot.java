4bf2da62209ebe8d65be6ada9babce32a10a9c88
TopiSenpai <tobi@strasserhof.com>
2021-02-27 19:56:57 +0100

    disabled success loggings for botlist apis

--- src/main/java/de/kittybot/kittybot/main/KittyBot.java
+++ src/main/java/de/kittybot/kittybot/main/KittyBot.java
@@ -33,6 +33,7 @@ public class KittyBot{
 		var botListHandler = new BLHBuilder()
 			.setUnavailableEventsEnabled(false)
 			.setDevModePredicate(jda -> Environment.is(Environment.DEVELOPMENT))
+			.setSuccessLoggingEnabled(false)
 			.addBotList(BotList.TOP_GG, Config.TOP_GG_TOKEN)
 			.addBotList(BotList.DBOATS, Config.DISCORD_BOATS_TOKEN)
 			.addBotList(BotList.BOTLIST_SPACE, Config.BOTLIST_SPACE_TOKEN)
